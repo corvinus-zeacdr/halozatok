8dcc388439f662c349742806499016f69b60efc7
corvinus-zeacdr <mate.kodobocz@stud.uni-corvinus.hu>
2021-03-04 11:23:39 +0100

    Negyedik gyakorlat

--- SzhGyak2/SzhGyak2/Startup.cs
+++ SzhGyak2/SzhGyak2/Startup.cs
@@ -30,7 +30,7 @@ namespace SzhGyak2
 
             DefaultFilesOptions options = new DefaultFilesOptions();
             options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("cv.html");
+            options.DefaultFileNames.Add("cv2.html");
             app.UseDefaultFiles(options);
             app.UseStaticFiles();
 
