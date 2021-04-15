b90fa6fba5ce526fecbc41ea12a853edb41367fa
corvinus-zeacdr <mate.kodobocz@stud.uni-corvinus.hu>
2021-04-15 14:44:00 +0200

    Nyolcadik gyakorlat

--- SzhGyak2/SzhGyak2/Startup.cs
+++ SzhGyak2/SzhGyak2/Startup.cs
@@ -16,6 +16,7 @@ namespace SzhGyak2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,12 @@ namespace SzhGyak2
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
